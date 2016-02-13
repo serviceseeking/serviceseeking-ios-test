fb50c52a0c77128a593ca2577a82c50a30704e54
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-02-13 17:39:00 +0800

    Added Lead and LeadsViewModel to the project

---
+++ swift/ios-test/Lead.swift
@@ -0,0 +1,27 @@
+//
+//  Lead.swift
+//  ios-test
+//
+//  Created by Garri Adrian Nablo on 2/13/16.
+//  Copyright © 2016 ServiceSeeking. All rights reserved.
+//
+
+import Foundation
+
+class Lead: NSObject {
+    
+    var name = ""
+    var jobDescription = ""
+    var username = ""
+    var suburb = ""
+    var biddingClosesOn = ""
+    var distanceAway = 0.0
+    var canQuote = false
+    var withinRange = false
+    var isNew = false
+    var isFeatured = false
+    var isUrgent = false
+    var isPrivate = false
+    var createdAt = ""
+    var updatedAt = ""
+}
---
+++ swift/ios-test/LeadsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LeadsViewModel.swift
+//  ios-test
+//
+//  Created by Garri Adrian Nablo on 2/13/16.
+//  Copyright © 2016 ServiceSeeking. All rights reserved.
+//
+
+import Foundation
+
+final class LeadsViewModel: NSObject {
+    
+    var leads = [Lead]()
+}
