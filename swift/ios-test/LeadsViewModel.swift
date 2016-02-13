45c495020ced80a234ea615a0c9ec83e572114e1
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-02-13 17:47:49 +0800

    Implemented LeadsViewModel

--- swift/ios-test/LeadsViewModel.swift
+++ swift/ios-test/LeadsViewModel.swift
@@ -7,8 +7,27 @@
 //
 
 import Foundation
+import Alamofire
+
+protocol LeadsViewModelDelegate: class {
+    func leadsViewModel(viewModel: LeadsViewModel, didFinishFetchingLeads leads: [Lead])
+}
 
 final class LeadsViewModel: NSObject {
     
     var leads = [Lead]()
+    weak var delegate: LeadsViewModelDelegate?
+    
+    func fetchLeads() {
+        let getLeadsRequest = ServiceSeekingAPI.Router.Leads
+        Alamofire.request(getLeadsRequest).response { (request, response, data, error) -> Void in
+            if let _ = error {
+                // do something for error
+            } else if let _ = data {
+                // convert JSON to Lead object
+                
+                self.delegate?.leadsViewModel(self, didFinishFetchingLeads: self.leads)
+            }
+        }
+    }
 }
