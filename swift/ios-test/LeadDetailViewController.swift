cd1f87f698190ec68d9dc83e4e07a1779ad54e1f
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-02-13 18:15:36 +0800

    Implemented LeadDetailViewController

--- swift/ios-test/LeadDetailViewController.swift
+++ swift/ios-test/LeadDetailViewController.swift
@@ -10,10 +10,56 @@ import UIKit
 
 class LeadDetailViewController: UIViewController {
 
+    private let viewModel: LeadDetailViewModel!
+    
+    init(lead: Lead) {
+        self.viewModel = LeadDetailViewModel(lead: lead)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.title = "Lead Detail"
+        self.view.backgroundColor = .whiteColor()
+        
+        let usernameLabel = UILabel()
+        usernameLabel.text = viewModel.lead.username
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(usernameLabel)
+        NSLayoutConstraint.activateConstraints([
+            NSLayoutConstraint(item: usernameLabel, attribute: .Top, relatedBy: .Equal, toItem: self.view, attribute: .Top, multiplier: 1.0, constant: 50.0),
+            NSLayoutConstraint(item: usernameLabel, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1.0, constant: 0.0)
+            ])
+        
+        let nameLabel = UILabel()
+        nameLabel.text = viewModel.lead.name
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activateConstraints([
+            NSLayoutConstraint(item: nameLabel, attribute: .Top, relatedBy: .Equal, toItem: usernameLabel, attribute: .Bottom, multiplier: 1.0, constant: 20.0),
+            NSLayoutConstraint(item: nameLabel, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1.0, constant: 0.0)
+            ])
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = viewModel.lead.jobDescription
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activateConstraints([
+            NSLayoutConstraint(item: descriptionLabel, attribute: .Top, relatedBy: .Equal, toItem: nameLabel, attribute: .Bottom, multiplier: 1.0, constant: 20.0),
+            NSLayoutConstraint(item: descriptionLabel, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1.0, constant: 0.0)
+            ])
+        
+        let suburbLabel = UILabel()
+        suburbLabel.text = viewModel.lead.suburb
+        suburbLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activateConstraints([
+            NSLayoutConstraint(item: suburbLabel, attribute: .Top, relatedBy: .Equal, toItem: descriptionLabel, attribute: .Bottom, multiplier: 1.0, constant: 20.0),
+            NSLayoutConstraint(item: suburbLabel, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1.0, constant: 0.0)
+            ])
     }
 
     override func didReceiveMemoryWarning() {
