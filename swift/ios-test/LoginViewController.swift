6bd55912a994dcf1deb729f6bd33c22c4351e9b0
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-02-13 14:26:42 +0800

    Added LoginViewController to the project

---
+++ swift/ios-test/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  ios-test
+//
+//  Created by Garri Adrian Nablo on 2/13/16.
+//  Copyright © 2016 ServiceSeeking. All rights reserved.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
