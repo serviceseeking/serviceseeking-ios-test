c538f7c1e8cb8cdedccae604b374c66c9c90d8bb
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-02-13 14:19:46 +0800

    Refactor default ViewController to RootViewController

---
+++ swift/ios-test/RootViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ios-test
+//
+//  Created by Justine on 19/01/2016.
+//  Copyright © 2016 ServiceSeeking. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    private var currentViewController: UIViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let leadsViewController = LeadsViewController()
+        let leadsNavigationController = UINavigationController(rootViewController: leadsViewController)
+        self.currentViewController = leadsNavigationController
+        
+        self.currentViewController.view.frame = self.view.bounds
+        self.addChildViewController(self.currentViewController)
+        self.view.addSubview(self.currentViewController.view)
+        self.currentViewController.didMoveToParentViewController(self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
