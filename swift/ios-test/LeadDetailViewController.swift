dd82159c8ec846beda7bee23ace702d4213420af
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-02-13 17:51:12 +0800

    Added LeadDetailViewController to the project

---
+++ swift/ios-test/LeadDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LeadDetailViewController.swift
+//  ios-test
+//
+//  Created by Garri Adrian Nablo on 2/13/16.
+//  Copyright © 2016 ServiceSeeking. All rights reserved.
+//
+
+import UIKit
+
+class LeadDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
