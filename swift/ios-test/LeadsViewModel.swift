a946cabb487cbf4174c4c2b8afa7cde5f0c5bbd6
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-02-15 01:58:06 +0800

    Convert json to Leads object

--- swift/ios-test/LeadsViewModel.swift
+++ swift/ios-test/LeadsViewModel.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 protocol LeadsViewModelDelegate: class {
     func leadsViewModel(viewModel: LeadsViewModel, didFinishFetchingLeads leads: [Lead])
@@ -21,11 +22,33 @@ final class LeadsViewModel: NSObject {
     func fetchLeads() {
         let getLeadsRequest = ServiceSeekingAPI.Router.Leads
         Alamofire.request(getLeadsRequest).response { (request, response, data, error) -> Void in
-            if let _ = error {
+            print(response!.allHeaderFields)
+            if let error = error {
                 // do something for error
-            } else if let _ = data {
-                // convert JSON to Lead object
-                
+                print(error)
+            } else if let data = data {
+                let json = JSON(data: data)
+                for (_, leadJSON) in json["data"] {
+                    let leadAttributes = leadJSON["attributes"]
+                    
+                    let lead = Lead()
+                    lead.name = leadAttributes["name"].stringValue
+                    lead.jobDescription = leadAttributes["description"].stringValue
+                    lead.username = leadAttributes["userName"].stringValue
+                    lead.suburb = leadAttributes["suburbName"].stringValue
+                    lead.biddingClosesOn = leadAttributes["biddingClosesOn"].stringValue
+                    lead.distanceAway = leadAttributes["distanceAway"].doubleValue
+                    lead.canQuote = leadAttributes["canQuote"].boolValue
+                    lead.withinRange = leadAttributes["withinRange"].boolValue
+                    lead.isNew = leadAttributes["new"].boolValue
+                    lead.isFeatured = leadAttributes["featured"].boolValue
+                    lead.isUrgent = leadAttributes["urgent"].boolValue
+                    lead.isPrivate = leadAttributes["private"].boolValue
+                    lead.createdAt = leadAttributes["createdAt"].stringValue
+                    lead.updatedAt = leadAttributes["updatedAt"].stringValue
+                    self.leads.append(lead)
+                }
+
                 self.delegate?.leadsViewModel(self, didFinishFetchingLeads: self.leads)
             }
         }
