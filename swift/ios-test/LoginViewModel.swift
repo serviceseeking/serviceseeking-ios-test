6c05be9f4665187fdced53a5b7b33e30b7308bfb
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-02-15 14:44:48 +0800

    Catch invalid information error

--- swift/ios-test/LoginViewModel.swift
+++ swift/ios-test/LoginViewModel.swift
@@ -22,17 +22,21 @@ final class LoginViewModel: NSObject {
                 NSNotificationCenter.defaultCenter().postNotificationName("FailLoginNotification", object: nil)
             } else if let data = data {
                 let json = JSON(data: data)
-                let userAttributes = json["attributes"]
-                
-                let user = User.sharedUser
-                user.name = userAttributes["name"].stringValue
-                user.email = userAttributes["email"].stringValue
-                user.status = userAttributes["status"].stringValue
-                user.phone = userAttributes["phone"].stringValue
-                user.createdAt = userAttributes["createdAt"].stringValue
-                user.updatedAt = userAttributes["updatedAt"].stringValue
-                user.token = json["meta"]["token"].stringValue
-                NSNotificationCenter.defaultCenter().postNotificationName("SuccessLoginNotification", object: nil)
+                if json["data"]["attributes"] != nil {
+                    let userAttributes = json["data"]["attributes"]
+                    let user = User.sharedUser
+                    user.name = userAttributes["name"].stringValue
+                    user.email = userAttributes["email"].stringValue
+                    user.status = userAttributes["status"].stringValue
+                    user.phone = userAttributes["phone"].stringValue
+                    user.createdAt = userAttributes["createdAt"].stringValue
+                    user.updatedAt = userAttributes["updatedAt"].stringValue
+                    user.token = json["meta"]["token"].stringValue
+                    NSNotificationCenter.defaultCenter().postNotificationName("SuccessLoginNotification", object: nil)
+                } else {
+                    let failLoginNotification = NSNotification(name: "FailLoginNotification", object: nil, userInfo: ["message": "The information entered is incorrect"])
+                    NSNotificationCenter.defaultCenter().postNotification(failLoginNotification)
+                }
             }
         }
     }
