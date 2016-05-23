88c588854aa86ebe866e18898824ebb3c9901897
Patriz <patriciac@agtechlabs.com>
2016-05-23 16:08:18 +0800

    Removed unused ViewController

---
+++ objc/ios-test/LeadDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  LeadDetailViewController.m
+//  ios-test
+//
+//  Created by Patricia Marie Cesar on 5/23/16.
+//  Copyright © 2016 ServiceSeeking. All rights reserved.
+//
+
+#import "LeadDetailViewController.h"
+
+@interface LeadDetailViewController ()
+
+@end
+
+@implementation LeadDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
