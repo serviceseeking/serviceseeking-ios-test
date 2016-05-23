38d72f60e4f62b8da600d455820623819504fa55
Patriz <patriciac@agtechlabs.com>
2016-05-23 20:41:59 +0800

    Display avatar, user name and user email

--- objc/ios-test/LeadsTableViewController.m
+++ objc/ios-test/LeadsTableViewController.m
@@ -13,6 +13,8 @@
 #import "Keys.h"
 #import "Lead.h"
 
+#import "User.h"
+
 #import "MBProgressHUD+Loading.h"
 
 static NSString * const segueIDLeadToLeadDetail = @"leadsToLeadDetail";
@@ -28,7 +30,8 @@ static NSString * const segueIDLeadToLeadDetail = @"leadsToLeadDetail";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [MBProgressHUD showLoadingHUDAddedTo:self.view labelText:@"Loading" detailLabelText:@"Please wait . . ."];
+    [MBProgressHUD showLoadingHUDAddedTo:self.tableView labelText:@"Loading" detailLabelText:@"Please wait . . ."];
+    self.tableView.tableFooterView = [UIView new];
     
     [[API sharedClient] getLeadsListingWithCompletionHandler:^(NSDictionary *responseDictionary) {
         NSArray *dataArray = responseDictionary[kData];
@@ -39,13 +42,10 @@ static NSString * const segueIDLeadToLeadDetail = @"leadsToLeadDetail";
         }
         
         dispatch_async(dispatch_get_main_queue(), ^{
-            [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+            [MBProgressHUD hideAllHUDsForView:self.tableView animated:YES];
             [self.tableView reloadData];
-            self.tableView.tableFooterView = [UIView new];
         });
     }];
-    
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -56,22 +56,36 @@ static NSString * const segueIDLeadToLeadDetail = @"leadsToLeadDetail";
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.leadArray.count;
+    return section == 0 ? 1 : self.leadArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"leadCellIdentifier" forIndexPath:indexPath];
     
-    Lead *lead = self.leadArray[indexPath.row];
-    cell.textLabel.text = lead.name?: @"";
-    cell.detailTextLabel.text = lead.userName ?: @"";
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    
-    return cell;
+    switch (indexPath.section) {
+        case 0: {
+            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"accountCellIdentifier" forIndexPath:indexPath];
+            cell.imageView.image = [UIImage imageNamed:@"avatar"];
+            cell.textLabel.text = [User sharedUserInstance].name;
+            cell.detailTextLabel.text = [User sharedUserInstance].email;
+            return cell;
+        }
+            break;
+        case 1: {
+            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"leadCellIdentifier" forIndexPath:indexPath];
+            Lead *lead = self.leadArray[indexPath.row];
+            cell.textLabel.text = lead.name?: @"";
+            cell.detailTextLabel.text = lead.userName ?: @"";
+            return cell;
+        }
+           break;
+        default:
+            return nil;
+            break;
+    }
 }
 
 #pragma mark - Navigation
