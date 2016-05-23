c5bd19a4602d0711a53677a629b8b4e9b0d522fe
Patriz <patriciac@agtechlabs.com>
2016-05-23 08:44:03 +0800

    Added service seeking API. Login successfully

---
+++ objc/ios-test/API.h
@@ -0,0 +1,21 @@
+//
+//  API.h
+//  ios-test
+//
+//  Created by Patricia Marie Cesar on 5/23/16.
+//  Copyright © 2016 ServiceSeeking. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^HTTPRequestCompletionBlock)(NSDictionary *responseDictionary);
+
+@interface API : NSObject
+
++ (instancetype)sharedClient;
+
+- (void)loginWithUsername:(NSString *)username
+                 password:(NSString *)password
+        completionHandler:(HTTPRequestCompletionBlock)completionHandler;
+
+@end
---
+++ objc/ios-test/API.m
@@ -0,0 +1,90 @@
+//
+//  API.m
+//  ios-test
+//
+//  Created by Patricia Marie Cesar on 5/23/16.
+//  Copyright © 2016 ServiceSeeking. All rights reserved.
+//
+
+#import "API.h"
+
+#import "Config.h"
+#import "URL.h"
+#import "Keys.h"
+
+#import "NSDictionary+Helper.h"
+
+static NSString * const HTTPHeaderAccept = @"application/vnd.api+json; version=1";
+static NSString * const HTTPHeaderAuthorization = @"Basic c3NzdGFnaW5nOnNzVDNzdDFuZyE=";
+static NSString * const HTTPHeaderContentType = @"application/vnd.api+json";
+
+
+static API *sharedClient;
+
+@interface API ()
+
+@property (strong, nonatomic) NSMutableURLRequest *apiRequest;
+
+@end
+
+@implementation API
+
++ (instancetype)sharedClient {
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedClient = [[API alloc] init];
+        sharedClient.apiRequest = [[NSMutableURLRequest alloc] init];
+        [sharedClient setRequestHeaders];
+    });
+    
+    return sharedClient;
+}
+
+#pragma mark - Login
+
+- (void)loginWithUsername:(NSString *)username
+                 password:(NSString *)password
+        completionHandler:(HTTPRequestCompletionBlock)completionHandler {
+    
+    NSDictionary *usernameAndPassword = @{kEmail: username, kPassword: password};
+    NSDictionary *typeAndAttributes = @{kType: @"user_sessions", kAttributes: usernameAndPassword};
+    NSMutableDictionary *parameters = @{kData: typeAndAttributes}.mutableCopy;
+    
+    [self requestWithMethod:@"POST" path:PATH_SIGN_IN parameters:parameters completionHandler:completionHandler];
+}
+
+#pragma mark - Private methods
+
+- (void)setRequestHeaders {
+    
+    [self.apiRequest addValue:HTTPHeaderAccept forHTTPHeaderField:@"Accept"];
+    [self.apiRequest addValue:HTTPHeaderAuthorization forHTTPHeaderField:@"Authorization"];
+    [self.apiRequest addValue:HTTPHeaderContentType forHTTPHeaderField:@"Content-Type"];
+    
+}
+
+- (void)requestWithMethod:(NSString *)method path:(NSString *)path
+               parameters:(NSDictionary *)parameters
+        completionHandler:(HTTPRequestCompletionBlock)completionHandler{
+    
+    self.apiRequest.HTTPMethod = method;
+    self.apiRequest.URL = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", URL_STAGING, path]];
+    self.apiRequest.HTTPBody = [parameters convertToData];
+    
+    [[[NSURLSession sharedSession] dataTaskWithRequest:self.apiRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                                         
+         if (data == nil) {
+             completionHandler(nil);
+         } else {
+             NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+    #if PRINT_RESPONSE == 1
+             NSLog(@"\nURL: %@ \nMETHOD: %@ \nPARAMS: %@ \nRESPONSE: %@",
+                   self.apiRequest.URL, self.apiRequest.HTTPMethod, parameters, responseDictionary);
+    #endif
+             completionHandler(responseDictionary);
+         }
+     }] resume];
+}
+
+@end
